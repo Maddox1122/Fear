98ff5d6c5607a72d2736b3a6a7830ced87648e84
Maddox <121097057+Maddox1122@users.noreply.github.com>
2024-04-12 11:44:45 +0200

    Installed tailwindcss and made the pages and NavBar and Footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import "./App.css";
+import NavBar from "./Components/NavBar/NavBar";
+import Footer from "./Components/Footer/Footer";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import NoPage from "./Pages/NoPage";
+import Producten from "./Pages/Producten";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Reviews from "./Pages/Reviews";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="producten" element={<Producten />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="reviews" element={<Reviews />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="terms" element={<Term />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
